6277636d6273f9494f5757a7cad9e84b5aef9cc5
ArunP1997 <apmdn1997@gmail.com>
2025-08-04 21:01:31 +0530

    Update linkedin url

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,7 +4,7 @@ function Contact() {
       <h2>Contact</h2>
       <p>Email - apmdn1997@gmail.com</p>
       <p>
-        LinkedIn - <a href="https://www.linkedin.com/in/arun-prasad-muralidharan-052a1a371/" className="text-blue-600 underline">MyLinkedIn</a>
+        LinkedIn - <a href="https://www.linkedin.com/in/arun-prasad-m-508a18377/" className="text-blue-600 underline">MyLinkedIn</a>
       </p>
     </section>
   );
